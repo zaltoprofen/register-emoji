2ccdb7bbae638312a0b8aade8b3e479735d37138
Y.Nakashima <nakashimayusuke@team-lab.com>
2017-06-21 18:16:01 +0900

    v0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='register-emoji',
-        version='0.1.0',
+        author='Yakov Nesterov',
+        author_email='pixy2001@gmail.com',
+        url='https://github.com/zaltoprofen/register-emoji',
+        version='0.1.1',
         packages=setuptools.find_packages(),
         install_requires=[
             'selenium',
